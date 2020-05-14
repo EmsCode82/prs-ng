60927c7420439eee2066a78eccafabdb6855b843
EmsCode82 <emsgithub@gmail.com>
2020-05-14 17:01:54 -0400

    .

---
+++ src/app/core/menu/menu.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'src/app/model/menu-item.class';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+  menuItems: MenuItem[] = [];
+  constructor() { }
+
+  ngOnInit(): void {
+    this.menuItems = [
+      new MenuItem("User","/users/list","User List")
+    ];
+  }
+
+}
